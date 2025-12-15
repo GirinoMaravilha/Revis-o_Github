7038589316b067567f339ea85491334d4cf00ae5
Lucas Salles <lubuntupython@gmail.com>
2025-12-15 19:10:33 -0300

    - refactor: Criado comentários para identificar melhor áreas do código contendo funções e classes.

--- test2.py
+++ test2.py
@@ -2,6 +2,7 @@
 Módulo para testar o comando REBASE do git
 """
 
+# Classes
 class teste2:
     """
     Classe para realizar um teste
@@ -15,9 +16,12 @@ class teste2:
     def mostra_p(self):
         return self.p
 
+
+# Função Main
 def main():
     t = teste2("Astride Gostosa")
     t.mostra_p()
+
 
 if __name__ == "__main__":
     main()
